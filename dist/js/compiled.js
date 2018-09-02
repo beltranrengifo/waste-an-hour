24093b990b0aed2e973c6446fdb415a38ab6b17b
Beltrán <merloc@gmail.com>
2018-09-02 12:11:24 +0200

    initial commit

---
+++ dist/js/compiled.js
@@ -0,0 +1,2 @@
+"use strict";function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}var Game=function a(){_classCallCheck(this,a),console.log("hi")};
+"use strict";
---
+++ src/index.js
@@ -0,0 +1,3 @@
+/* DEV WATCHING: npx babel src --watch --out-file dist/js/compiled.js */
+/* PROD WATCHING: BABEL_ENV=production npx babel src --watch --out-file dist/js/compiled.js */
+/* PROD COMPILE: BABEL_ENV=production npx babel src --out-file dist/js/compiled.js */
